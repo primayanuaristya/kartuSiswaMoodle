6fddc5736cdadacb73033f587a1678c1be6a92fd
primayanuaristya <primayanuaristya@gmail.com>
2021-11-30 08:54:34 +0000

    Menambahkan time

--- newConfig.php
+++ newConfig.php
@@ -1,11 +1,12 @@
 <?php
 require '../config.php';
-$newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,$CFG->dbpass,$CFG->dbname);
+$newConn = mysqli_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass, $CFG->dbname);
 // $newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,'123',$CFG->dbname);
 // Check connection
 
 if (mysqli_connect_errno($newConn)) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-  }
-  $time=time();
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+$time = time();
