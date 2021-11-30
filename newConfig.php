004f1cd574443ce7078368bae6f012268e1832bd
primayanuaristya <primayanuaristya@gmail.com>
2021-11-30 08:56:45 +0000

    tambahan

--- newConfig.php
+++ newConfig.php
@@ -1,7 +1,7 @@
 <?php
 require '../config.php';
 $newConn = mysqli_connect($CFG->dbhost, $CFG->dbuser, $CFG->dbpass, $CFG->dbname);
-// $newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,'123',$CFG->dbname);
+
 // Check connection
 
 if (mysqli_connect_errno($newConn)) {
