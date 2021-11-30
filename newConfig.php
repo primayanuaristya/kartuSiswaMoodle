2e99f1b77a40840fcda86d4dba11732bac2a7ead
primayanuaristya <primayanuaristya@gmail.com>
2021-11-30 08:50:24 +0000

    some_message

---
+++ newConfig.php
@@ -0,0 +1,10 @@
+<?php
+require '../config.php';
+$newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,$CFG->dbpass,$CFG->dbname);
+// $newConn = mysqli_connect($CFG->dbhost,$CFG->dbuser,'123',$CFG->dbname);
+// Check connection
+
+if (mysqli_connect_errno($newConn)) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+  }
